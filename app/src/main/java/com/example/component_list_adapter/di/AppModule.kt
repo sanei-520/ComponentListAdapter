067a38e4f863dcfd1c98de819d519f16eedbc2e8
FuFunG <37580095+FuFunG@users.noreply.github.com>
2021-04-14 20:43:34 +0800

    firebase analytics with uat / prod

---
+++ app/src/main/java/com/example/component_list_adapter/di/AppModule.kt
@@ -0,0 +1,19 @@
+package com.example.component_list_adapter.di
+
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideFirebaseAnalytics() = Firebase.analytics
+
+}
--- app/src/main/java/com/example/component_list_adapter/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/component_list_adapter/ui/main/MainViewModel.kt
@@ -2,17 +2,20 @@ package com.example.component_list_adapter.ui.main
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
+import com.example.component_list_adapter.BuildConfig
 import com.example.component_list_adapter.network.model.Component
 import com.example.component_list_adapter.repository.ComponentRepo
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.logEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    componentRepo: ComponentRepo
+    componentRepo: ComponentRepo,
+    firebaseAnalytics: FirebaseAnalytics
 ) : ViewModel() {
 
     val components = MutableLiveData<List<Component>>()
@@ -20,6 +23,10 @@ class MainViewModel @Inject constructor(
     init {
         viewModelScope.launch {
             components.value = componentRepo.getComponents()
+            firebaseAnalytics.logEvent("component_call") {
+                param("env", BuildConfig.APPLICATION_ID)
+                param("base_url", BuildConfig.BASE_URL)
+            }
         }
     }
 
